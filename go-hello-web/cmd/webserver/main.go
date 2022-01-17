5d21f0413896b1fe278cf24f09fe0487a6ed4d8e
Sean Li <sean.li@hennge.com>
2022-01-17 17:31:12 +0900

    feat: add roll20 function

--- go-hello-web/cmd/webserver/main.go
+++ go-hello-web/cmd/webserver/main.go
@@ -13,10 +13,11 @@ const listenPort = 8080
 
 func main() {
 	diceSDK := dice.New()
-	service := webservice.New(diceSDK)
+	service := webservice.New(diceSDK, diceSDK)
 
 	http.HandleFunc("/ping", service.Ping)
 	http.HandleFunc("/roll", service.Roll)
+	http.HandleFunc("/roll20", service.Roll20)
 
 	log.Printf("Listening on %d\n", listenPort)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", listenPort), nil))
--- go-hello-web/internal/webservice/webservice.go
+++ go-hello-web/internal/webservice/webservice.go
@@ -12,16 +12,23 @@ type SixSidedDie interface {
 	Roll6() (int, error)
 }
 
+// TwentySidedDie represents any object that can roll a twenty sided dice for us.
+type TwentySidedDie interface {
+	Roll20() (int, error)
+}
+
 // New returns a new initialised Webserver.
-func New(die SixSidedDie) *WebService {
+func New(die SixSidedDie, twenty TwentySidedDie) *WebService {
 	return &WebService{
-		sixSidedDie: die,
+		sixSidedDie:    die,
+		twentySidedDie: twenty,
 	}
 }
 
 // WebService provides a set of web endpoints that handle HTTP requests.
 type WebService struct {
-	sixSidedDie SixSidedDie
+	sixSidedDie    SixSidedDie
+	twentySidedDie TwentySidedDie
 }
 
 // Ping handles an HTTP ping request, returning pong.
@@ -37,6 +44,23 @@ func (wb *WebService) Ping(w http.ResponseWriter, r *http.Request) {
 // Roll handles an HTTP dice roll request, returning a random number between 1-6 for us.
 func (wb *WebService) Roll(w http.ResponseWriter, r *http.Request) {
 	roll, err := wb.sixSidedDie.Roll6()
+	if err != nil {
+		log.Println(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	_, err = io.WriteString(w, fmt.Sprintf("%d\n", roll))
+	if err != nil {
+		log.Println(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+// Roll20 handles an HTTP dice roll request, returning a random number between 1-20 for us.
+func (wb *WebService) Roll20(w http.ResponseWriter, r *http.Request) {
+	roll, err := wb.twentySidedDie.Roll20()
 	if err != nil {
 		log.Println(err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
